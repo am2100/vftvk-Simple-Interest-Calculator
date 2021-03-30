5e55e154aff2f535d8781ba25fc89555c04d3552
am2100 <jimnoble@xjjz.co.uk>
2021-03-30 11:38:27 +0000

    initial compute function

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+
+    var principal = p;
+    var rate = r;
+    var years = y;
+
+    var interest = principal * years * rate / 100;
+
+    var today = new Date();
+    var this_year = today.getFullYear;
+    var final_year = this_year + years;
+
+    alert(principal, rate, years, interest, today, this_year, final_year);
 }
         
