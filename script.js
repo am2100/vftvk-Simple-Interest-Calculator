b12378d548b8076c3078a90c4009555b2dbfd9a9
am2100 <j@jebn.me>
2021-03-31 09:02:07 +0100

    Remove immediate intParse of user input to allow for regex validation

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
 function compute()
 {
-    // collect page elements for easy reference
-    p_el = document.getElementById("principal");
-    r_el = document.getElementById("rate");
-    y_el = document.getElementById("years");
+  // collect page elements for easy reference
+  var p_el = document.getElementById("principal");
+  var r_el = document.getElementById("rate");
+  var y_el = document.getElementById("years");
+
+  // collect user data for validation
+  var principal = p_el.value;
+  var rate      = r_el.value;
+  var years     = y_el.value;
+
+  // parse input to integers to make calculations easier to read
+  var principal = parseInt(p_el.value);
+  var rate      = parseInt(r_el.value);
+  var years     = parseInt(y_el.value);
+
+  // verify principal is greater than 0 and not an empty string
+  // if not, create an Alert
+  // delete existing input value
+  // set focus to relevant input field
+  if(principal < 1 || principal == ""){
+    alert("Enter a positive number");
+    p_el.value = "";
+    p_el.focus();
+  }
+  else {
 
     result_el = document.getElementById("result");
+    // Do the necessary calculations
+    var total_return = principal * years * rate / 100;
 
-    // parse input to integers to make calculations easier to read
-    var principal = parseInt(p_el.value);
-    var rate      = parseInt(r_el.value);
-    var years     = parseInt(y_el.value);
+    var today      = new Date();
+    var this_year  = parseInt(today.getFullYear());
+    var final_year = this_year + years;
 
-    // verify principal is greater than 0 and not an empty string
-    // if not, create an Alert
-    // delete existing input value
-    // set focus to relevant input field
-    if(principal < 1 || principal == ""){
-        alert("Enter a positive number");
-        p_el.value = "";
-        p_el.focus();
-    }
-    else {
-        // Do the necessary calculations
-        var total_return = principal * years * rate / 100;
+    // Format output string
+    var result_output = "If you deposit <mark>" + principal + "</mark><br>";
+    result_output += "at an interest rate of <mark>" + rate + "%</mark><br>";
+    result_output += "You will receive an amount of <mark>" + total_return + "</mark>,<br>";
+    result_output += "in the year <mark>" + final_year +"</mark>";
 
-        var today      = new Date();
-        var this_year  = parseInt(today.getFullYear());
-        var final_year = this_year + years;
-
-        // Format output string
-        var result_output = "If you deposit <mark>" + principal + "</mark><br>";
-        result_output += "at an interest rate of <mark>" + rate + "%</mark><br>";
-        result_output += "You will receive an amount of <mark>" + total_return + "</mark>,<br>";
-        result_output += "in the year <mark>" + final_year +"</mark>";
-
-        result_el.innerHTML = result_output;
-    }
+    result_el.innerHTML = result_output;
+  }
 }
 
 function display_rate(){
-    var rate = document.getElementById("rate").value;
-    var rate_el = document.getElementById("rate-info");
+  var rate = document.getElementById("rate").value;
+  var rate_el = document.getElementById("rate-info");
 
-    rate_el.innerHTML = rate + " %";
+  rate_el.innerHTML = rate + " %";
 }
 
