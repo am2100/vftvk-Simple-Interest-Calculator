86d75676bf856de342be5d754c6257da0690559b
am2100 <j@jebn.me>
2021-03-31 13:30:58 +0100

    tidy up vars. parseFloat for rate. Update references to report id

--- script.js
+++ script.js
@@ -1,46 +1,60 @@
 function compute()
 {
+  // numeric string regex
+  // matches a string that only contains numeric characters
+  // match fails for:
+  // empty strings
+  // strings with non-numeric characters
+  // negative numbers
+  var regex = /^\d+$/;
+
   // collect page elements for easy reference
   var p_el = document.getElementById("principal");
-  var r_el = document.getElementById("rate");
-  var y_el = document.getElementById("years");
+  var r_el, y_el, report_el;
 
   // collect user data for validation
   var principal = p_el.value;
-  var rate      = r_el.value;
-  var years     = y_el.value;
+  var rate, years;
 
-  // parse input to integers to make calculations easier to read
-  var principal = parseInt(p_el.value);
-  var rate      = parseInt(r_el.value);
-  var years     = parseInt(y_el.value);
+  // declare other vars
+  var total_interest, today, this_year, final_year, report_output;
 
-  // verify principal is greater than 0 and not an empty string
+  // verify principal is a number greater than 0
   // if not, create an Alert
   // delete existing input value
   // set focus to relevant input field
-  if(principal < 1 || principal == ""){
+  if(!regex.test(principal) || parseInt(principal) < 1){
     alert("Enter a positive number");
     p_el.value = "";
     p_el.focus();
   }
   else {
+    r_el = document.getElementById("rate");
+    y_el = document.getElementById("years");
 
-    result_el = document.getElementById("result");
-    // Do the necessary calculations
-    var total_return = principal * years * rate / 100;
+    // store output elements
+    report_el = document.getElementById("report");
 
-    var today      = new Date();
-    var this_year  = parseInt(today.getFullYear());
-    var final_year = this_year + years;
+    // parse input to ints and floats for calculations
+    principal = parseInt(p_el.value);
+    rate      = parseFloat(r_el.value);
+    years     = parseInt(y_el.value);
+
+    // Calculate interest
+    total_interest = principal * years * rate / 100;
+
+    // Calculate No. Years end date
+    today      = new Date();
+    this_year  = parseInt(today.getFullYear());
+    final_year = this_year + years;
 
     // Format output string
-    var result_output = "If you deposit <mark>" + principal + "</mark><br>";
-    result_output += "at an interest rate of <mark>" + rate + "%</mark><br>";
-    result_output += "You will receive an amount of <mark>" + total_return + "</mark>,<br>";
-    result_output += "in the year <mark>" + final_year +"</mark>";
+    report_output = "If you deposit <mark>" + principal + "</mark><br>";
+    report_output += "at an interest rate of <mark>" + rate + "%</mark><br>";
+    report_output += "You will receive an amount of <mark>" + total_interest + "</mark>,<br>";
+    report_output += "in the year <mark>" + final_year +"</mark>";
 
-    result_el.innerHTML = result_output;
+    report_el.innerHTML = report_output;
   }
 }
 
